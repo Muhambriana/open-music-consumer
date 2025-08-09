8f0513c407164354bae5af8b25e90e60b16bfd98
muham <muhambriana.31@gmail.com>
2025-08-09 21:07:35 +0700

    refactor: enhance Listener to include playlist details in email

--- src/Listener.js
+++ src/Listener.js
@@ -10,8 +10,15 @@ class Listener {
     try {
       const { playlistId, targetEmail } = JSON.parse(message.content.toString());
 
+      const playlist = await this._playlistsService.getPlaylistById(playlistId);
       const playlistSongs = await this._playlistsService.getPlaylistSongs(playlistId);
-      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlistSongs));
+      const finalData = {
+        playlist: {
+          ...playlist,
+          songs: playlistSongs,
+        },
+      };
+      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(finalData));
       console.log(result);
     } catch (error) {
       console.log(error);
--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -5,9 +5,23 @@ class PlaylistsService {
     this._pool = new Pool();
   }
 
+  async getPlaylistById(playlistId) {
+    const query = {
+      text: 'SELECT public_id as id, name FROM playlists WHERE public_id = $1',
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new Error('Playlist Not Found');
+    }
+
+    return result.rows[0];
+  }
+
   async getPlaylistSongs(playlistId) {
     const query = {
-      text: `SELECT s.*
+      text: `SELECT s.public_id AS id, s.title, s.performer
       FROM songs s
       JOIN playlist_songs ps ON ps.song_id = s.rec_id
       JOIN playlists p ON p.rec_id = ps.playlist_id
